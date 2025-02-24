f867a538401cccb1ead2c29fe42f50507840f1f0
monolino <jul.wuersch@gmail.com>
2025-02-24 16:34:25 +0100

    added method getUserById

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -96,5 +97,15 @@ public class UserService {
 
     return userFound;
   }
+
+  public User getUserById(Long id) {
+    Optional<User> userFound = userRepository.findById(id);
+
+    if (userFound.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found");
+    }
+    return userFound.get();
+  }
+
 
 }
