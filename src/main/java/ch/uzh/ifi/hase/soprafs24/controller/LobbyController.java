1f29a18ca94a66ffb3bfd815eb77729c01b09cff
Anja Lindenmann <anja.lindenmann@uzh.ch>
2025-04-01 11:53:51 +0200

    Create endpoints POST/lobbies, GET/lobbies/{lobbyId} #37

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
@@ -0,0 +1,41 @@
+package ch.uzh.ifi.hase.soprafs24.controller;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+public class LobbyController {
+
+    private final LobbyService lobbyService;
+
+    LobbyController(LobbyService lobbyService) {
+        this.lobbyService = lobbyService;
+    }
+
+    @PostMapping("/lobbies")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public LobbyGetDTO createLobby(@RequestBody LobbyPostDTO LobbyPostDTO) {
+
+        Lobby userInput = DTOMapper.INSTANCE.convertLobbyPostDTOtoEntity(LobbyPostDTO);
+
+        Lobby createdLobby = lobbyService.createLobby(userInput);
+
+        return DTOMapper.INSTANCE.convertEntityToLobbyGetDTO(createdLobby);
+    }
+
+    @GetMapping("/lobbies/{lobbyId}")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public LobbyGetDTO getLobby(@PathVariable Long lobbyId) {
+
+        Lobby lobby = lobbyService.getLobbyById(lobbyId);
+
+        return DTOMapper.INSTANCE.convertEntityToLobbyGetDTO(lobby);
+    }
+}
