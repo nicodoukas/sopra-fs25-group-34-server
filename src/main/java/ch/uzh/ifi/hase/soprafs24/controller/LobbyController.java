c458ea0cf4b96e7e3f218b7f1bdd77c7912151c9
monolino <jul.wuersch@gmail.com>
2025-04-01 14:04:35 +0200

    Created endpoint GET/lobbies/{lobbyId}/users for  #42.

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
@@ -1,12 +1,21 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class LobbyController {
@@ -38,4 +47,24 @@ public class LobbyController {
 
         return DTOMapper.INSTANCE.convertEntityToLobbyGetDTO(lobby);
     }
+
+    @GetMapping("/lobbies/{lobbyId}/users")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<UserGetDTO> getAllMembers(@PathVariable Long lobbyId) {
+        Lobby lobby = lobbyService.getLobbyById(lobbyId);
+
+        // fetch all users in the internal representation
+        List<User> users = lobbyService.getMembers(lobby);
+        List<UserGetDTO> userGetDTOs = new ArrayList<>();
+
+        // convert each user to the API representation
+        for (User user : users) {
+            userGetDTOs.add(DTOMapper.INSTANCE.convertEntityToUserGetDTO(user));
+        }
+
+        return userGetDTOs;
+    }
+
+
 }
