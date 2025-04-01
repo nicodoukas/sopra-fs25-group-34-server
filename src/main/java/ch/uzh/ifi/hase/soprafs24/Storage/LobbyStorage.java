51466062fec0cff44485574b4ae7a546d8d05eef
monolino <jul.wuersch@gmail.com>
2025-04-01 13:39:38 +0200

    Created Class where the Lobby instances are stored.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Storage/LobbyStorage.java
@@ -0,0 +1,24 @@
+package ch.uzh.ifi.hase.soprafs24.Storage;
+
+import org.springframework.stereotype.Component;
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Component
+public class LobbyStorage {
+
+    private final Map<Long, Lobby> lobbies = new ConcurrentHashMap<>();
+
+    public Lobby addLobby(Lobby lobby) {
+        lobbies.put(lobby.getLobbyId(), lobby);
+        return lobby;
+    }
+
+    public Lobby getLobbyBId(Long lobbyId) {
+        return lobbies.get(lobbyId);
+    }
+
+}
