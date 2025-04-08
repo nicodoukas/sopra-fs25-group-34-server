55547f0d6c831997fe4b7b25ba99c934dde0860b
monolino <jul.wuersch@gmail.com>
2025-04-08 18:54:45 +0200

    GameStorage added. #47

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/storage/GameStorage.java
@@ -0,0 +1,32 @@
+package ch.uzh.ifi.hase.soprafs24.storage;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@Component
+public class GameStorage {
+
+    private final Map<Long, Game> games = new ConcurrentHashMap<>();
+
+    public Game addGame(Game game) {
+        if (game == null || game.getGameId() == null) {
+            throw new IllegalArgumentException("Game or gameId cannot be null");
+        }
+        games.put(game.getGameId(), game);
+        return game;
+    }
+
+    public Game getGameById(Long gameId) {
+        if (!games.containsKey(gameId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Game with ID " + gameId + " not found");
+        }
+        return games.get(gameId);
+    }
+}
+
