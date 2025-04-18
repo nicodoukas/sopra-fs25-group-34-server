accc7301307cfc4f06cde12a4ace4d55e5d633f9
monolino <jul.wuersch@gmail.com>
2025-04-18 11:15:07 +0200

    Test POST/lobbies/invite/{userId}. #86

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyControllerPostTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyControllerPostTest.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyPostDTO;
@@ -7,14 +8,18 @@ import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
 import ch.uzh.ifi.hase.soprafs24.service.UserService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Date;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
@@ -34,6 +39,17 @@ public class LobbyControllerPostTest {
 
     @MockBean
     private UserService userService;
+
+    private Lobby lobby;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        lobby = new Lobby();
+        lobby.setLobbyId(2L);
+        lobby.setLobbyName("Our awesome Lobby");
+    }
 
     //POST /lobbies
     @Test
@@ -62,6 +78,26 @@ public class LobbyControllerPostTest {
         mockMvc.perform(postRequest)
             .andExpect(status().isCreated())
             .andExpect(jsonPath("$.lobbyName", is(lobby.getLobbyName())));
+    }
+
+    // POST/lobbies/invite/{userId}
+    @Test
+    public void inviteUser_success() throws Exception {
+
+        User user = new User();
+        user.setId(1L);
+
+        given(lobbyService.getLobbyById(2L)).willReturn(lobby);
+        given(userService.getUserById(1L)).willReturn(user);
+        given(lobbyService.inviteUserToLobby(user, 2L)).willReturn(user);
+
+        MockHttpServletRequestBuilder postRequest = post("/lobbies/invite/1")
+                .content("2") //lobbyId
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(postRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId().intValue())));
     }
 
 }
