97bb12fe4c1b96be9060fd8cf98745d7e01e5197
monolino <jul.wuersch@gmail.com>
2025-04-18 18:54:56 +0200

    Added all LobbyService tests. #90

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/LobbyServiceTest.java
@@ -0,0 +1,88 @@
+package ch.uzh.ifi.hase.soprafs24.service;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs24.storage.LobbyStorage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LobbyServiceTest {
+    @Mock
+    private LobbyStorage lobbyStorage;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private LobbyService lobbyService;
+
+    private Lobby testLobby;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+
+        testLobby = new Lobby();
+        testLobby.setLobbyName("testLobby");
+        testLobby.setLobbyId(1L);
+
+        Mockito.when(lobbyStorage.addLobby(Mockito.any())).thenReturn(testLobby);
+
+    }
+
+    @Test
+    public void createLobby_success() {
+        Lobby createdLobby = lobbyService.createLobby(testLobby);
+        assertEquals(createdLobby.getLobbyId(), testLobby.getLobbyId());
+        assertEquals(createdLobby.getLobbyName(), testLobby.getLobbyName());
+    }
+
+    @Test
+    public void inviteUserToLobby_success() {
+        User user = new User();
+        user.setId(1L);
+        User addedUser = lobbyService.inviteUserToLobby(user, testLobby.getLobbyId());
+        assertEquals(addedUser.getId(), user.getId());
+        assertTrue(addedUser.getOpenLobbyInvitations().contains(testLobby.getLobbyId()));
+    }
+
+    @Test
+    public void manageLobbyRequest_accepted_success() {
+        User user = new User();
+        user.setId(1L);
+        user.addLobbyInvitation(testLobby.getLobbyId());
+        Mockito.when(userService.getUserById(user.getId())).thenReturn(user);
+        Lobby lobby = lobbyService.manageLobbyRequest(testLobby,user.getId(),true);
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+        Mockito.verify(userRepository, Mockito.times(1)).flush();
+        assertEquals(testLobby.getLobbyId(), lobby.getLobbyId());
+        assertTrue(lobby.getMembers().contains(user));
+        assertTrue(user.getOpenLobbyInvitations().isEmpty());
+    }
+
+    @Test
+    public void manageLobbyRequest_declined_success() {
+        User user = new User();
+        user.setId(1L);
+        user.addLobbyInvitation(testLobby.getLobbyId());
+        Mockito.when(userService.getUserById(user.getId())).thenReturn(user);
+        Lobby lobby = lobbyService.manageLobbyRequest(testLobby,user.getId(),false);
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
+        Mockito.verify(userRepository, Mockito.times(1)).flush();
+        assertEquals(testLobby.getLobbyId(), lobby.getLobbyId());
+        assertTrue(lobby.getMembers().isEmpty());
+        assertTrue(user.getOpenLobbyInvitations().isEmpty());
+    }
+
+
+}
