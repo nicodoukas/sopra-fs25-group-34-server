1c6e3777bddc054772c2a7c961867073bfc83e08
monolino <jul.wuersch@gmail.com>
2025-04-21 19:44:36 +0200

    Created Guess entity with checkGuess. #71

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Guess.java
@@ -0,0 +1,32 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import java.io.Serializable;
+
+public class Guess implements Serializable {
+
+    private String guessedTitle;
+    private String guessedArtist;
+    private Player player;
+
+    public void setGuessedTitle(String guessedTitle) {
+        if (guessedTitle != null) {this.guessedTitle = guessedTitle.trim();}
+        else {this.guessedTitle = null;}
+    }
+    public String getGuessedTitle() {return this.guessedTitle;}
+
+    public void setGuessedArtist(String guessedArtist) {
+        if (guessedArtist != null) {this.guessedArtist = guessedArtist.trim();}
+        else {this.guessedArtist = null;}
+    }
+    public String getGuessedArtist() {return this.guessedArtist;}
+
+    public void setPlayer(Player player) {this.player = player;}
+    public Player getPlayer() {return this.player;}
+
+    public boolean checkGuess(Game game) {
+        SongCard songCard = game.getCurrentRound().getSongCard();
+        if (songCard.getArtist().equals(this.guessedArtist)) {return true;}
+        else if (songCard.getTitle().equals(this.guessedTitle)) {return true;}
+        else {return false;}
+    }
+}
